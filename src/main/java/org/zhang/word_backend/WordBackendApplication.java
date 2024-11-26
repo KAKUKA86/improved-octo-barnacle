dea8ef0e371073a53f42eacf96db4e713416124f
张邦宸 <zhangbang2002@foxmail.com>
2024-11-27 00:46:02 +0800

    初始化项目

---
+++ src/main/java/org/zhang/word_backend/WordBackendApplication.java
@@ -0,0 +1,13 @@
+package org.zhang.word_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zhang/word_backend/WordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zhang.word_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
